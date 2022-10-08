b6e83277d72aa750bbe9715a4a2d5b0ac3417c60
unknown <ansaebrahim@gmail.com>
2022-10-08 16:12:54 +0530

    first commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Helloooo --- World")
+    
+
+    li = ["Allen","Ansa","Allu"]
+
+    context = {'names':li}
+
+    return render(request, 'index.html',context=context)
+
+
+def new_one(request):
+    return HttpResponse("This is New one")  
